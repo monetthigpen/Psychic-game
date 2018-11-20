519f7c2734ebea1b5d6fcb639a4b44be33fa9992
Monetthigpen <monet.thigpen@gmail.com>
2018-11-19 22:47:44 -0500

    yes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,63 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var letterTooguess = ["v","y","e","d","h","k"];
+var lettersGuessed = [];
 
- var winsElement =html.getElementById("wins");
- var lossesElement = html.getElementById("losses");
- var guessesLeftElement = html.getElementById("guessesLeft");
- var lettersGuessedElement = html.getElementById("lettersGuessed");
- var computerGuess = letterTooguess[Math.floor(Math.random() * letterTooguess.length ) ];
+
+
+var winsElement = document.getElementById("wins");
+var lossesElement = document.getElementById("losses");
+var guessesLeftElement = document.getElementById("guessesLeft");
+var lettersGuessedElement = document.getElementById("lettersGuessed");
+var computerGuess = letterTooguess[Math.floor(Math.random() * letterTooguess.length ) ];
 
 document.onkeyup = function(event){
-   
-    
-    var userGuess = event.key;
-    //var userGuessElement.textContent = userGuess;
-    
-    
-        if(userGuess === computerGuess){
-            wins++; 
-            winsElement.textContent = wins;
-        }
-        else{
-            losses++;
-            lossesElement.textContent = losses;
 
-        }  
+
+    var userGuess = event.key;
+    userGuess = userGuess.toLowerCase();
+
+
+   //lettersGuessedElement.textContent = userGuess;
+   
+   lettersGuessed.push(userGuess);
+   lettersGuessedElement.textContent = lettersGuessed.join(", ");
+            //console.log(lettersGuessed); 
+    //document.getElementById("lettersGuessed").getAttribute("userGuess")
+
+       if(userGuess === computerGuess){
+         wins++; 
+         winsElement.textContent = wins;
+         lettersGuessed = [];
+        }
+       if (guessesLeft === 0){
+            //alert("Opps! You have exceeded the number of guesses. You lose! Try again!")
+            losses++ ;
+            lossesElement.textContent = losses;
+            guessesLeft = 9;
+            guessesLeft.reload(
+                guessesLeft >= 0);
+          for(var i = 0; i < lettersGuessed.length; i++){
+              lettersGuessed.clear(
+                lettersGuessed[i] = 9)
+          };
+
+            
+
+        }
+
+        else {
+         guessesLeft-- < 9;
+         guessesLeftElement.textContent = guessesLeft;
+         
+        }
+        // .push[]
+        
+
+
    
 
 };
+
 
 
